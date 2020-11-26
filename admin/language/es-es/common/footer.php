9c91b65444f51ea173cfd6cf205bc56784e09405
sergio77772 <sergio_77772@hotmail.com>
2020-11-26 16:42:32 -0300

    ....

--- admin/language/es-es/common/footer.php
+++ admin/language/es-es/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = 'Diseño y Desarrollo <a href="https://www.blimop.tech/" target="_blank">Blimop</a> MERCADITO © 2020 ';
+$_['text_footer']  = 'Diseño y Desarrollo <a href="https://www.blimop.tech/" target="_blank">Blimop</a> AMBIENT° © 2020 ';
 $_['text_version'] = '';
