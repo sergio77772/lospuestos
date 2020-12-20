6162b3963a23dff06e60e844aa321c1b2fdf0d60
sergio77772 <sergio_77772@hotmail.com>
2020-12-20 19:18:03 -0300

    .....

--- catalog/controller/api/customer.php
+++ catalog/controller/api/customer.php
@@ -112,11 +112,10 @@ class ControllerApiCustomer extends Controller {
  			unset($this->session->data['reward']);
  			unset($this->session->data['voucher']);
  			unset($this->session->data['vouchers']);
- 			$json['success'] = $this->language->get('text_success');
+ 			$json['success'] = 'operacion exitosa';
  				$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 
 		}
 
-	}
 }
